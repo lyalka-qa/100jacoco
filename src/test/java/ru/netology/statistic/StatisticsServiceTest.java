e3c6fdb20d7f8005533b3701d3ade38419683511
Alena Filatova <ppasd2005@yandex.ru>
2021-05-20 00:29:29 +0300

    100% code coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,10 +9,10 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
-        long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomes);
 
         assertEquals(expected, actual);
     }
